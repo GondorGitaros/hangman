91cdf8e69d87ed0ec3da148069e0a0a6fd9029c3
GondorGitaros <csaszardaniel81@gmail.com>
2024-02-23 22:15:08 +0100

    fully working hangman in 1 try

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import random
 
-words = ['cat', 'window', 'defenestrate', 'dog', ]
+words = ['cat', 'window', 'dog', ]
 word = random.choice(words)
+lword = []
+
+for i in range(len(word)):
+    print('_', end=' ')
+    lword.append('_')
+print()
+
+counter = 0
+guessed = False
+
+while not guessed:
+    guess = input('Guess a letter: ')
+    for i in range(len(lword)):
+        if guess == word[i]:
+            lword[i] = guess
+            print(lword[i], end=' ')
+        else:
+            print(lword[i], end=' ')
+    if '_' not in lword:
+        guessed = True
+        print('\nYou win!')
+    print()
